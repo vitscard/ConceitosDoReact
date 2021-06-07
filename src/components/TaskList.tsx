e27b6883f97a775ca13757a2867cb85926d4b706
unknown <vit.scard@gmail.com>
2021-06-07 19:29:55 -0300

    Desafio Finalizado

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,29 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!!newTaskTitle) {
+      const newTask: Task = {
+        id: Math.random(),
+        title: newTaskTitle,
+        isComplete: false,        
+      }
+      setTasks([...tasks, newTask])
+      setNewTaskTitle('')      
+    } else {
+      return;
+    }  
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const taskFilter = tasks.map(task => task.id === id ? {
+      ...task, isComplete: !task.isComplete
+    }: task);
+    setTasks(taskFilter);  
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const taskFilter = tasks.filter(t => t.id !== id);
+    setTasks(taskFilter);
   }
 
   return (
